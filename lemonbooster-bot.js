3d26edf716b3dcba84034acf45baeab932ab0b04
bonino97 <juanbonino97@gmail.com>
2020-05-01 19:24:37 -0300

    Update lemonbooster-bot.js

--- lemonbooster-bot.js
+++ lemonbooster-bot.js
@@ -9,32 +9,48 @@ const botKey = '1075658805:AAEyt1oljdw-DvDSU7pN691fnCetUNYeuUA';
 const bot = new Telegraf(botKey);
 
 bot.start((ctx) => {
-    ctx.reply('Welcome!');
+    ctx.reply(`
+    
+    ~Welcome to LemonBooster Bot~
+
+    Send me a message with /help to know the functions that i can execute! 👍
+
+    `);
 });
 
 bot.help( (ctx) => {
-    ctx.reply('/Enumeration ---> Executes Enumeration')
-    ctx.reply('/Monitoring ---> Executes Monitoring')
+    ctx.reply(`
+    
+    /Enumeration ---> Executes Enumeration 
+    /Monitoring ---> Executes Monitoring :performing_arts:
+
+    `);
 });
 
 bot.command(['Enumeration', 'enumeration', 'ENUMERATION'], (ctx) =>{
     fetch('http://monitoring.lemonsec.com/Enumeration')
         .then(res => {
             if(res.ok){
-                ctx.reply('Ok!')
+                ctx.reply('Subdomain Enumeration Finish Correctly!')
+            } else {
+                ctx.reply('Subdomain Enumeration Fail...')
+                ctx.reply('Response: ', res.msg)
             }
         })
-        .catch(err => console.log(err))
+        .catch(err => ctx.reply(err))
 });
 
 bot.command(['Monitoring','monitoring','MONITORING'], (ctx) =>{
     fetch('http://monitoring.lemonsec.com/Monitoring')
         .then(res => {
             if(res.ok){
-                ctx.reply('Ok!')
+                ctx.reply('Monitoring Finish Correctly!')
+            } else {
+                ctx.reply('Monitoring Fail...')
+                ctx.reply('Response: ', res.msg)
             }
         })
-        .catch(err => console.log(err))
+        .catch(err => ctx.reply(err))
 });
 
 bot.launch();
